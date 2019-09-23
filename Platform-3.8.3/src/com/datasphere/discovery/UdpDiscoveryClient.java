92602a05a3e3542d55aa1c8f0e48a33420a80a92
theseus yang <theseusyang@192.168.0.103>
2019-09-23 22:26:09 +0800

    add discovery modules

---
+++ Platform-3.8.3/src/com/datasphere/discovery/UdpDiscoveryClient.java
@@ -0,0 +1,112 @@
+package com.datasphere.discovery;
+
+import org.apache.log4j.*;
+import com.datasphere.uuid.*;
+import com.datasphere.uuid.UUID;
+import com.datasphere.security.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+/*
+ * 通过 UDP 发现客户端
+ */
+public class UdpDiscoveryClient
+{
+    public static final String DISCOVERY_REQUEST_HEADER = "DISCOVER WEBACTION SERVER";
+    private static final int MAX_DATAGRAM_PACKET_SIZE = 1024;
+    private static final int TIMEOUT_IN_MSEC = 5000;
+    private static final Logger logger;
+    private final int serverDiscoveryPort;
+    private final UUID saltId;
+    private final String clusterName;
+    
+    public UdpDiscoveryClient(final int port, final String cluster, final UUID salt) {
+        this.serverDiscoveryPort = port;
+        this.saltId = salt;
+        this.clusterName = cluster;
+    }
+    
+    public String discoverWebActionServers() {
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket();
+            socket.setBroadcast(true);
+            final byte[] request = this.getDiscoveryMessage().getBytes("UTF-8");
+            this.sendRequestToBroadcastAddress(socket, request);
+            this.sendRequestToAllInterfaces(socket, request);
+            final byte[] recvBuf = new byte[1024];
+            final DatagramPacket recvPacket = new DatagramPacket(recvBuf, recvBuf.length);
+            socket.setSoTimeout(5000);
+            int maxAttempts = 10;
+            while (maxAttempts-- > 0) {
+                socket.receive(recvPacket);
+                final String resp = new String(recvPacket.getData()).trim();
+                String payload = null;
+                if (this.saltId != null) {
+                    payload = WASecurityManager.decrypt(resp, this.saltId.toEightBytes());
+                }
+                else {
+                    payload = resp;
+                }
+                final StringTokenizer tokenizer = new StringTokenizer(payload, "\n");
+                if (tokenizer.countTokens() > 1) {
+                    final String header = tokenizer.nextToken().trim();
+                    if ("WEBACTION SERVER NODE".equals(header)) {
+                        return tokenizer.nextToken().trim();
+                    }
+                    continue;
+                }
+            }
+            return null;
+        }
+        catch (SocketTimeoutException ste) {
+            UdpDiscoveryClient.logger.info((Object)"No WebAction Server could be discovered within the specified timeout. Either specify Server Address or check the network setting to enable broadcast");
+            return null;
+        }
+        catch (Exception e) {
+            UdpDiscoveryClient.logger.warn((Object)"No WebAction Server could be discovered due to exception", (Throwable)e);
+            return null;
+        }
+        finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+    
+    private void sendRequestToBroadcastAddress(final DatagramSocket socket, final byte[] request) throws IOException {
+        final DatagramPacket packet = new DatagramPacket(request, request.length, InetAddress.getByName("255.255.255.255"), this.serverDiscoveryPort);
+        socket.send(packet);
+    }
+    
+    private void sendRequestToAllInterfaces(final DatagramSocket socket, final byte[] request) throws IOException {
+        final Enumeration<NetworkInterface> intefaces = NetworkInterface.getNetworkInterfaces();
+        while (intefaces.hasMoreElements()) {
+            final NetworkInterface networkInterface = intefaces.nextElement();
+            if (!networkInterface.isLoopback()) {
+                if (!networkInterface.isUp()) {
+                    continue;
+                }
+                for (final InterfaceAddress iAddress : networkInterface.getInterfaceAddresses()) {
+                    final InetAddress broadcastAddress = iAddress.getBroadcast();
+                    if (broadcastAddress == null) {
+                        continue;
+                    }
+                    final DatagramPacket sendpacket = new DatagramPacket(request, request.length, broadcastAddress, this.serverDiscoveryPort);
+                    socket.send(sendpacket);
+                }
+            }
+        }
+    }
+    
+    private String getDiscoveryMessage() {
+        final StringBuffer buffer = new StringBuffer("DISCOVER WEBACTION SERVER");
+        buffer.append("\nCluster:" + this.clusterName);
+        buffer.append("\nRequestTime:" + System.currentTimeMillis());
+        return buffer.toString();
+    }
+    
+    static {
+        logger = Logger.getLogger((Class)UdpDiscoveryClient.class);
+    }
+}
---
+++ Platform-3.8.3/src/com/datasphere/discovery/UdpDiscoveryServer.java
@@ -0,0 +1,150 @@
+package com.datasphere.discovery;
+
+import org.apache.log4j.*;
+import com.datasphere.uuid.*;
+import com.datasphere.uuid.UUID;
+import com.datasphere.metaRepository.*;
+import java.net.*;
+import java.nio.channels.*;
+import java.util.*;
+import com.datasphere.security.*;
+import java.security.*;
+import java.io.*;
+import java.nio.*;
+
+/*
+ * 通过 UDP 发现服务器
+ */
+public class UdpDiscoveryServer implements Runnable
+{
+    public static final String DISCOVERY_RESPONSE_HEADER = "WEBACTION SERVER NODE";
+    private static final int MAX_DATAGRAM_PACKET_SIZE = 1024;
+    private static final Logger logger;
+    private final int boundPort;
+    private final String hazelcastBindAddress;
+    private final int hazelcastBindPort;
+    private final UUID salt;
+    private final String clusterName;
+    private DatagramChannel channel;
+    
+    public UdpDiscoveryServer(final int boundPort, final UUID salt) {
+        this(boundPort, salt, HazelcastSingleton.getBindingInterface(), ((InetSocketAddress)HazelcastSingleton.get().getLocalEndpoint().getSocketAddress()).getPort(), HazelcastSingleton.getClusterName());
+    }
+    
+    public UdpDiscoveryServer(final int boundPort, final UUID salt, final String serverIP, final int serverPort, final String cluster) {
+        this.boundPort = boundPort;
+        this.salt = salt;
+        this.hazelcastBindAddress = serverIP;
+        this.hazelcastBindPort = serverPort;
+        this.clusterName = cluster;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            final Selector selector = Selector.open();
+            (this.channel = DatagramChannel.open()).configureBlocking(false);
+            this.channel.register(selector, 1);
+            try {
+                this.channel.socket().bind(new InetSocketAddress(this.boundPort));
+                this.channel.register(selector, 1, new ClientRecord());
+                while (!Thread.currentThread().isInterrupted()) {
+                    if (selector.select() == 0) {
+                        continue;
+                    }
+                    final Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
+                    while (keyIter.hasNext()) {
+                        final SelectionKey key = keyIter.next();
+                        if (key.isReadable()) {
+                            this.handleRead(key);
+                        }
+                        if (key.isValid() && key.isWritable()) {
+                            this.handleWrite(key);
+                        }
+                        keyIter.remove();
+                    }
+                }
+            }
+            finally {
+                if (this.channel != null) {
+                    this.channel.close();
+                    this.channel = null;
+                }
+            }
+        }
+        catch (ClosedByInterruptException ex) {}
+        catch (Exception e) {
+            UdpDiscoveryServer.logger.warn((Object)"UdpDiscoveryServer stopping to respond to Discovery requests", (Throwable)e);
+        }
+    }
+    // 验证发现请求
+    private boolean verifyDiscoveryRequest(final String discoveryPayload) throws ClosedByInterruptException {
+        final StringTokenizer tokenizer = new StringTokenizer(discoveryPayload, "\n");
+        if (tokenizer.countTokens() < 2) {
+            return false;
+        }
+        final String header = tokenizer.nextToken();
+        final String requestedCluster = tokenizer.nextToken();
+        if (header == null || header.trim().isEmpty()) {
+            return false;
+        }
+        if (!"DISCOVER WEBACTION SERVER".equals(header)) {
+            return false;
+        }
+        if (requestedCluster == null || requestedCluster.trim().isEmpty()) {
+            return false;
+        }
+        final StringTokenizer clusterTokenizer = new StringTokenizer(requestedCluster, ":");
+        if (clusterTokenizer.countTokens() < 2) {
+            return false;
+        }
+        final String clusterHeader = clusterTokenizer.nextToken();
+        final String requestedClusterName = clusterTokenizer.nextToken();
+        return this.clusterName.equals(requestedClusterName);
+    }
+    
+    private String formatResponse() throws UnsupportedEncodingException, GeneralSecurityException {
+        final StringBuffer buffer = new StringBuffer("WEBACTION SERVER NODE \n");
+        buffer.append(this.hazelcastBindAddress + ":" + this.hazelcastBindPort);
+        if (this.salt != null) {
+            return WASecurityManager.encrypt(buffer.toString(), this.salt.toEightBytes());
+        }
+        return buffer.toString();
+    }
+    // 处理读操作
+    private void handleRead(final SelectionKey key) throws IOException {
+        final DatagramChannel channel = (DatagramChannel)key.channel();
+        final ClientRecord clientRecord = (ClientRecord)key.attachment();
+        clientRecord.buffer.clear();
+        clientRecord.clientAddress = channel.receive(clientRecord.buffer);
+        if (clientRecord.clientAddress != null && this.verifyDiscoveryRequest(new String(clientRecord.buffer.array()))) {
+            key.interestOps(4);
+        }
+    }
+    // 处理写操作
+    private void handleWrite(final SelectionKey key) throws GeneralSecurityException, IOException {
+        final DatagramChannel channel = (DatagramChannel)key.channel();
+        final ClientRecord clientRecord = (ClientRecord)key.attachment();
+        clientRecord.buffer.clear();
+        clientRecord.buffer.put(this.formatResponse().getBytes("UTF-8"));
+        clientRecord.buffer.flip();
+        final int bytesSent = channel.send(clientRecord.buffer, clientRecord.clientAddress);
+        if (bytesSent != 0) {
+            key.interestOps(1);
+        }
+    }
+    
+    static {
+        logger = Logger.getLogger((Class)UdpDiscoveryServer.class);
+    }
+    
+    private static class ClientRecord
+    {
+        public SocketAddress clientAddress;
+        public ByteBuffer buffer;
+        
+        private ClientRecord() {
+            this.buffer = ByteBuffer.allocate(1024);
+        }
+    }
+}
