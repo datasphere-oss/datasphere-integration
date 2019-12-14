31c4953debd2a6be848ef51703bd0376662fc84e
theseus yang <theseusyang@192.168.1.21>
2019-12-14 16:54:36 +0800

    add oracle datatype

--- SchemaMigration-4.0/src/com/datasphere/db/dao/impl/OracleDao.java
+++ SchemaMigration-4.0/src/com/datasphere/db/dao/impl/OracleDao.java
@@ -1,10 +1,14 @@
 package com.datasphere.db.dao.impl;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -20,18 +24,37 @@ import com.datasphere.db.dao.AbstractBaseDao;
 import com.datasphere.db.entity.Column;
 import com.datasphere.db.entity.Schema;
 import com.datasphere.db.entity.Table;
-import com.datasphere.db.migration.scheduler.impl.Oracle2SnappyMigrationScheduler;
+import com.datasphere.db.migration.scheduler.impl.Oracle2HiveMigrationScheduler;
 
 import oracle.jdbc.pool.OracleConnectionPoolDataSource;
 
 public class OracleDao extends AbstractBaseDao {
 	
-	OracleConnectionPoolDataSource oracleConnectionPoolDataSource;
-
 	private final static String SQL_SELECT_SCHEMA_AND_TABLE = "SELECT OWNER,TABLE_NAME FROM DBA_TABLES WHERE OWNER NOT IN ('MDSYS','OUTLN','FLOWS_FILES','SYSTEM','EXFSYS','APEX_030200','DBSNMP','ORDSYS','APPQOSSYS','XDB','ORDDATA','SYS','WMSYS')";
 	private final static String SQL_SELECT_TABLE_ATTR = "SELECT COLUMN_NAME, DATA_TYPE, DATA_LENGTH, NULLABLE ,DATA_PRECISION ,DATA_SCALE FROM DBA_TAB_COLUMNS WHERE  OWNER = ? AND  TABLE_NAME = ? ORDER BY COLUMN_ID";
 	private final static String SQL_SELECT_COMMENTS = "SELECT COLUMN_NAME,COMMENTS FROM dba_col_comments WHERE TABLE_NAME = ? AND OWNER = ?";
 
+	private final static String SQL_SELECT_TOTAL_BLOCKS = "SELECT sum(blocks) as total_blocks FROM dba_extents WHERE OWNER=upper(?) and SEGMENT_NAME = upper(?)";
+	private final static String SQL_SELECT_OBJECT_ID = "SELECT data_object_id FROM dba_objects WHERE owner=upper(?) and object_name=upper(?)";
+	private final static String SQL_SELECT_DBA_EXTENDS = "select file_id,block_id from dba_extents  where owner=upper(?) AND SEGMENT_NAME=upper(?) ORDER BY FILE_ID,BLOCK_ID";
+	private final static String SQL_SELECT_ROWID_CREATE = "select DBMS_ROWID.ROWID_CREATE (1,?,?,?,?) rowid from dual";
+	
+	OracleConnectionPoolDataSource oracleConnectionPoolDataSource;
+	
+	OracleConnectionPoolDataSource getDataSource() throws SQLException {
+		
+		if(oracleConnectionPoolDataSource == null) {
+			OracleConfig oracleConfig = getConfig();
+			oracleConnectionPoolDataSource = new OracleConnectionPoolDataSource();
+			oracleConnectionPoolDataSource.setDriverType("thin");
+			oracleConnectionPoolDataSource.setServerName(oracleConfig.getHost());
+			oracleConnectionPoolDataSource.setPortNumber(oracleConfig.getPort());
+			oracleConnectionPoolDataSource.setDatabaseName(oracleConfig.getDatabaseName());
+			oracleConnectionPoolDataSource.setUser(oracleConfig.getUser());
+			oracleConnectionPoolDataSource.setPassword(oracleConfig.getPassword());
+		}
+		return oracleConnectionPoolDataSource;
+	}
 	/**
 	 * 获取表字段的注释
 	 * 
@@ -101,7 +124,9 @@ public class OracleDao extends AbstractBaseDao {
 
 	@Override
 	public List<Table> getTables() throws Exception{
-		try(Connection conn = getConnection();PreparedStatement pst = conn.prepareStatement(SQL_SELECT_TABLE_ATTR)) {
+		Connection conn = getConnection();
+		PreparedStatement pst = conn.prepareStatement(SQL_SELECT_TABLE_ATTR);
+		try{
 			List<Table> tables = new LinkedList<Table>();
 			HashMap<String,Set<String>> schemaTables = getSchemaTables();
 			for(String schemaName : schemaTables.keySet()){
@@ -116,7 +141,7 @@ public class OracleDao extends AbstractBaseDao {
 						String columnName = set.getString(1);
 						Boolean isNotNull = "Y".equals(set.getString(4)) ? true : false;
 						String columnType = set.getString(2);
-						Integer columnLength = set.getInt(3);
+						Long columnLength = set.getLong(3);
 						Integer precision = set.getInt(5);
 						Integer scale = set.getInt(6);
 						column.setName(columnName);
@@ -134,6 +159,9 @@ public class OracleDao extends AbstractBaseDao {
 				}
 			}
 			return tables;
+		}finally {
+			pst.close();
+			conn.close();
 		}
 	}
 
@@ -153,7 +181,7 @@ public class OracleDao extends AbstractBaseDao {
 			
 			int row = 0;
 			int col = columns.size();
-			ResultSet set = stat.executeQuery("select count(*) from " + table.getSchema() + "." + table.getName() + "");
+			ResultSet set = stat.executeQuery("select count(1) from " + table.getSchema() + "." + table.getName() + "");
 			if(set.next()) {
 				row = Long.valueOf(set.getLong(1)).intValue();
 			}
@@ -177,9 +205,9 @@ public class OracleDao extends AbstractBaseDao {
 			return data;
 		}
 	}
-	
+	// 通过类型获得结果集
 	private Object resultsetGetBytype(String type,ResultSet rs,int index) throws SQLException{
-		if(type.startsWith(Oracle2SnappyMigrationScheduler.TYPE_TIMESTAMP)){
+		if(type.startsWith(Oracle2HiveMigrationScheduler.TYPE_TIMESTAMP)){
 			return rs.getTimestamp(index);
 		}
 		Object data = null;
@@ -207,20 +235,7 @@ public class OracleDao extends AbstractBaseDao {
 		}
 	}
 	
-	OracleConnectionPoolDataSource getDataSource() throws SQLException {
-		
-		if(oracleConnectionPoolDataSource == null) {
-			OracleConfig oracleConfig = getConfig();
-			oracleConnectionPoolDataSource = new OracleConnectionPoolDataSource();
-			oracleConnectionPoolDataSource.setDriverType("thin");
-			oracleConnectionPoolDataSource.setServerName(oracleConfig.getHost());
-			oracleConnectionPoolDataSource.setPortNumber(oracleConfig.getPort());
-			oracleConnectionPoolDataSource.setDatabaseName(oracleConfig.getDatabaseName());
-			oracleConnectionPoolDataSource.setUser(oracleConfig.getUser());
-			oracleConnectionPoolDataSource.setPassword(oracleConfig.getPassword());
-		}
-		return oracleConnectionPoolDataSource;
-	}
+	
 
 	@Override
 	public void checkTable(Table table) throws Exception {}
@@ -228,10 +243,6 @@ public class OracleDao extends AbstractBaseDao {
 	@Override
 	public void putData(Table table, Object[][] data) throws Exception {}
 	
-	private final static String SQL_SELECT_TOTAL_BLOCKS = "SELECT sum(blocks) as total_blocks FROM dba_extents WHERE OWNER=upper(?) and SEGMENT_NAME = upper(?)";
-	private final static String SQL_SELECT_OBJECT_ID = "SELECT data_object_id FROM dba_objects WHERE owner=upper(?) and object_name=upper(?)";
-	private final static String SQL_SELECT_DBA_EXTENDS = "select file_id,block_id from dba_extents  where owner=upper(?) AND SEGMENT_NAME=upper(?) ORDER BY FILE_ID,BLOCK_ID";
-	private final static String SQL_SELECT_ROWID_CREATE = "select DBMS_ROWID.ROWID_CREATE (1,?,?,?,?) myRowid from dual";
 	
 	private String genRowid(int[] sqlParam) throws SQLException {
 		try(Connection conn = getConnection();){
@@ -242,7 +253,7 @@ public class OracleDao extends AbstractBaseDao {
 			pst.setInt(4, sqlParam[3]);
 			ResultSet rset = pst.executeQuery();
 			rset.next();
-			String generatedRowid = rset.getString("myRowid");
+			String generatedRowid = rset.getString("rowid");
 			rset.close();
 			pst.close();
 			return generatedRowid;
@@ -316,4 +327,267 @@ public class OracleDao extends AbstractBaseDao {
 		
 		return oraRowsRangeList;
 	}
+	
+	
+	
+	   public String getOracleType(String sourceType, String sourceLength)
+	   {
+	     String oraType = "";
+	     switch (sourceType.toUpperCase())
+	     {
+	     case "VARCHAR2":
+	     case "VARCHAR":
+	     case "SYSNAME":
+	     case "VARGRAPHIC":
+	     case "VARG":
+	     case "UNIQUEIDENTIFIER":
+	       oraType = "VARCHAR2(" + sourceLength + ")";
+	       break;
+	     case "NVARCHAR2":
+	     case "NCHAR VARYING":
+	     case "NVARCHAR":
+	       oraType = "NVARCHAR2(" + sourceLength + ")";
+	       break;
+	     case "NCHAR":
+	       oraType = "NCHAR(" + sourceLength + ")";
+	       break;
+	     case "CHAR":
+	     case "GRAPHIC":
+	     case "CHARACTER":
+	       oraType = "CHAR(" + sourceLength + ")";
+	       break;
+	     case "CLOB":
+	     case "TEXT":
+	       oraType = "CLOB";
+	       break;
+	     case "NTEXT":
+	       oraType = "NCLOB";
+	       break;
+	     case "MONEY":
+	     case "SMALLMONEY":
+	     case "DECFLOAT":
+	     case "TINYINT":
+	     case "SMALLINT":
+	     case "INT":
+	     case "BIGINT":
+	     case "FLOAT":
+	     case "REAL":
+	     case "DECIMAL":
+	     case "NUMERIC":
+	     case "NUMBER":
+	     case "INTEGER":
+	     case "DOUBLE":
+	       oraType = "NUMBER";
+	       break;
+	     case "DATE":
+	     case "DATETIME":
+	     case "TIME":
+	       oraType = "DATE";
+	       break;
+	     case "TIME WITH TIME ZONE":
+	       oraType = "TIME WITH TIME ZONE";
+	       break;
+	     case "TIMESTAMP WITH TIME ZONE":
+	       oraType = "TIMESTAMP WITH TIME ZONE";
+	       break;
+	     case "TIMESTAMP WITH LOCAL TIME ZONE":
+	       oraType = "TIMESTAMP WITH LOCAL TIME ZONE";
+	       break;
+	     case "TIMESTAMP":
+	     case "TIMESTMP":
+	       oraType = "TIMESTAMP";
+	     }
+	 
+	     return oraType;
+	   }
+	 
+	   public void addFieldValue(String fieldType, int fieldPosition, String fieldValue, PreparedStatement commandStatement, SimpleDateFormat inputDateFormat) throws Exception
+	   {
+	     switch (fieldType.toUpperCase())
+	     {
+	     	case "NVARCHAR2":
+	     	case "NCHAR VARYING":
+	     	case "NVARCHAR":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, -9);
+		       }
+		       else
+		       {
+		         commandStatement.setString(fieldPosition, fieldValue);
+		       }
+		       break;
+	     	case "VARCHAR2":
+	     	case "VARCHAR":
+	     	case "SYSNAME":
+	     	case "VARGRAPHIC":
+	     	case "VARG":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 12);
+		       }
+		       else
+		       {
+		         commandStatement.setString(fieldPosition, fieldValue);
+		       }
+		       break;
+	     	case "CLOB":
+	     	case "TEXT":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 2005);
+		       }
+		       else
+		       {
+		         commandStatement.setString(fieldPosition, fieldValue);
+		       }
+		       break;
+	     	case "NTEXT":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 2011);
+		       }
+		       else
+		       {
+		         commandStatement.setString(fieldPosition, fieldValue);
+		       }
+		       break;
+	     	case "NCHAR":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, -15);
+		       }
+		       else
+		       {
+		         commandStatement.setString(fieldPosition, fieldValue);
+		       }
+		       break;
+	     	case "UNIQUEIDENTIFIER":
+	     	case "CHAR":
+	     	case "GRAPHIC":
+	     	case "CHARACTER":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 1);
+		       }
+		       else
+		       {
+		         commandStatement.setString(fieldPosition, fieldValue);
+		       }
+		       break;
+	     	case "NUMERIC":
+	     	case "NUMBER":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 2);
+		       }
+		       else
+		       {
+		         commandStatement.setBigDecimal(fieldPosition, new BigDecimal(fieldValue));
+		       }
+		       break;
+		     case "MONEY":
+		     case "SMALLMONEY":
+		     case "DECFLOAT":
+		     case "DECIMAL":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 3);
+		       }
+		       else
+		       {
+		         commandStatement.setBigDecimal(fieldPosition, new BigDecimal(fieldValue));
+		       }
+		       break;
+		     case "DATE":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 91);
+		       }
+		       else
+		       {
+		         commandStatement.setDate(fieldPosition, new java.sql.Date(inputDateFormat.parse(fieldValue).getTime()));
+		       }
+		       break;
+		     case "TIME":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 92);
+		       }
+		       else
+		       {
+		         commandStatement.setTime(fieldPosition, new Time(inputDateFormat.parse(fieldValue).getTime()));
+		       }
+		       break;
+		     case "DATETIME":
+		     case "TIME WITH TIME ZONE":
+		     case "TIMESTAMP WITH TIME ZONE":
+		     case "TIMESTAMP WITH LOCAL TIME ZONE":
+		     case "TIMESTAMP":
+		     case "TIMESTMP":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 93);
+		       }
+		       else
+		       {
+		         commandStatement.setTimestamp(fieldPosition, new Timestamp(inputDateFormat.parse(fieldValue).getTime()));
+		       }
+		       break;
+		     case "FLOAT":
+		     case "DOUBLE":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 8);
+		       }
+		       else
+		       {
+		         commandStatement.setDouble(fieldPosition, Double.parseDouble(fieldValue));
+		       }
+		       break;
+		     case "BIGINT":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, -5);
+		       }
+		       else
+		       {
+		         commandStatement.setLong(fieldPosition, Long.parseLong(fieldValue));
+		       }
+		       break;
+		     case "TINYINT":
+		     case "SMALLINT":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 5);
+		       }
+		       else
+		       {
+		         commandStatement.setShort(fieldPosition, Short.parseShort(fieldValue));
+		       }
+		       break;
+		     case "INT":
+		     case "INTEGER":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 4);
+		       }
+		       else
+		       {
+		         commandStatement.setInt(fieldPosition, Integer.parseInt(fieldValue));
+		       }
+		       break;
+		     case "REAL":
+		       if ((fieldValue.isEmpty()) || (fieldValue.equals("null")) || (fieldValue.equals("NULL")))
+		       {
+		         commandStatement.setNull(fieldPosition, 7);
+		       }
+		       else
+		       {
+		         commandStatement.setFloat(fieldPosition, Float.parseFloat(fieldValue));
+		       }
+		       break;
+	     }
+	   }
+	
 }
